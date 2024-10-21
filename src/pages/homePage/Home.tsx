53cd84db054eefac6adab005aeb33f5e8e2023d8
Dmytro Andrushko <dmytroandrushko@Noutbuk-Dmytro.local>
2024-10-21 14:51:47 +0300

    Add listener

--- src/pages/homePage/Home.tsx
+++ src/pages/homePage/Home.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
 import "./styles.css";
 
 const Home = () => {
-  const send = ()=>{
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      console.log(event);
+    });
+  }, []);
+
+  const send = () => {
     const iFrame = document.getElementById("embeddedAppsmith") as any;
     //Send the message in postMessage
     //replace the "<Appsmith_hosted_url>" with your Appsmith domain
-    iFrame.contentWindow.postMessage('Hello from Meta4', "https://appsmith-dev.srv.festcloud.ai/app/my-first-application/employee-67038f786c61df06c4ecdf28?embed=true");
+    iFrame.contentWindow.postMessage(
+      "Hello from Meta4",
+      "https://appsmith-dev.srv.festcloud.ai/app/my-first-application/employee-67038f786c61df06c4ecdf28?embed=true"
+    );
   };
-
 
   return (
     <>
