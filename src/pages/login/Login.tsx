1c25466bcf3750dcc3e6e66dd34150a1cd95b135
Dmytro Andrushko <dmytroandrushko@Noutbuk-Dmytro.local>
2024-10-21 15:26:27 +0300

    Add

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,17 +4,16 @@ const Login = () => {
   const send = () => {
     debugger
     const iFrame = document.getElementById("embeddedAppsmith") as any;
-    //Send the message in postMessage
-    //replace the "<Appsmith_hosted_url>" with your Appsmith domain
     iFrame.contentWindow.postMessage(
       "Hello from Meta4",
-      "https://appsmith-dev.srv.festcloud.ai/app/my-first-application/login-page-670628006c61df06c4ecdfa1?embed=true"
+      "*"
     );
   };
 
   return (
     <div className="login-page">
       <button onClick={send}>Send</button>
+      
       <iframe
         id="embeddedAppsmith"
         src="https://appsmith-dev.srv.festcloud.ai/app/my-first-application/login-page-670628006c61df06c4ecdfa1"
